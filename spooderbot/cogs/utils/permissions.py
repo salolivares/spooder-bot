1aa42041b5f0b0e1296ef23443faccbe4bbf8af1
Sal Olivares <salisonhismac@gmail.com>
2016-07-13 00:08:53 -0700

    refactored some code

--- spooderbot/cogs/utils/permissions.py
+++ spooderbot/cogs/utils/permissions.py
@@ -1,9 +1,16 @@
 from discord.ext import commands
 
+authorizedUsers = [
+    "140350353521639424",  # Sal
+    "140208896726925312"   # Elijah
+]
 
 def checkIfOwner(message):
-    return message.author.id == '140350353521639424' or message.author.id == '140208896726925312'
+    """A helper function used by isOwner()."""
+    return message.author.id in authorizedUsers
 
 
 def isOwner():
+    """Checks if the author of the message is the owner of the server.
+    Essentially checks if author is a superadmin."""
     return commands.check(lambda ctx: checkIfOwner(ctx.message))
